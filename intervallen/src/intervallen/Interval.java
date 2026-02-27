9f9c07d4dfc8a16170a402831d9cc920cbc9fe3f
Liselotte Baeyens <liselotte.baeyens@student.kuleuven.be>
2026-02-27 10:36:40 +0100

    documentatie

--- intervallen/src/intervallen/Interval.java
+++ intervallen/src/intervallen/Interval.java
@@ -4,29 +4,61 @@
 package intervallen;
 
 /**
+ * Elke instantie van deze klasse slaat een 
+ * interval met gehele ondergrens en bovengrens op. 
  * 
+ * @invar De bovengrens is groter dan of gelijk aan 
+ * de ondergrens 
+ * |getOndergrens()<=getBovengrens()
+ * @invar De lengte is gelijk aan de bovengrens min 
+ * de ondergrens 
+ * |getLengte()==getBovengrens() -
+ * |getOndergrens()
  */
 public class Interval {
+	/**
+	 * @invar|0<=lengte
+	 */
 	private int ondergrens;
 	private int lengte;
 	
-	int getOndergrens() 
-	{return this.ondergrens;}
+	public int getOndergrens() 
+	{return ondergrens;}
 	
-	int getBovengrens() 
-	{return this.ondergrens + this.lengte; }
+	public int getBovengrens() 
+	{return ondergrens + lengte; }
 	
+	public int getLengte() {return lengte;}
+	
+	/**
+	 * Stelt het object in met de gegeven waarden. //meestal geen informele documentatie// 
+	 * @pre|ondergrens <= bovengrens
+	 * @post|getOndergrens()==ondergrens
+	 * @post|getBovengrens()==bovengrens
+	 */
 	Interval(int ondergrens, int bovengrens){
-		this.ondergrens = ondergrens;
-		this.lengte=bovengrens-ondergrens;
+		this.ondergrens = ondergrens; // hier kan je this niet weglaten want parameter met dezelfde naam// 
+		lengte=bovengrens-ondergrens;
 	}
+	/**
+	 * @mutates|this
+	 * @post|getOndergrens()==ondergrens
+	 * @post|getLengte()==old(getLengte())
+	 */
 	
-	void setOndergrens(int ondergrens) {
+	public void setOndergrens(int ondergrens) {
 		this.ondergrens=ondergrens;
 		
 	}
-	void setBovengrens(int bovengrens) {
-		this.lengte = bovengrens-this.ondergrens;
+	/**
+	 * @pre|getOndergrens()<=bovengrens
+	 * @mutates|this
+	 * @post|getBovengrens()==bovengrens
+	 * @post|getOndergrens()==old(getOndergrens())
+	 */
+	
+	public void setBovengrens(int bovengrens) {
+		lengte = bovengrens-ondergrens;
 	}
 	
 }
